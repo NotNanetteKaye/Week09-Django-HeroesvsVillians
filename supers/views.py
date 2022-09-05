dd28adad0dad2eb8ac449df9e1030a5493f989a6
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-09-05 18:15:13 -0500

    Created New View Function in Supers Views.py

--- supers/views.py
+++ supers/views.py
@@ -3,15 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Super
 from .serializers import SuperSerializer
+from django.shortcuts import get_object_or_404
+
+from supers import serializers
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(['GET'])
+def supers_detail(request, pk):
+    supers = get_object_or_404(Super, pk=pk)
+    serializer = SuperSerializer(supers)
+    return Response(serializer.data)
+    
