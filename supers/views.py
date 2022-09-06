7f152c41233db435ecdd6bf02ee8c97096db8315
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-09-05 23:12:27 -0500

    Created Super_Types App + Created Relationship with Super App

--- supers/views.py
+++ supers/views.py
@@ -10,8 +10,14 @@ from supers import serializers
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
+        super_type_name = request.query_params.get('super_type')
+        print(super_type_name)
+
+        queryset = Super.objects.all()
+
+        if super_type_name:
+            queryset = queryset.filter(super_type_id__type=super_type_name)
+        serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
